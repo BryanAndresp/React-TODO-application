9f8e9d9c670a45980cb33ace7052bcf16ce3f545
omegamaximus10 <bryan_2904@hotmail.com>
2021-07-26 16:08:49 +0000

    pepe

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export const Home = () => {
+	let [tasks, setTasks] = useState([]);
+	let [inputValue, setInputValue] = useState("");
+	//const [listLength, setListLength] = useState(0);
 
-//create your first component
-export function Home() {
+	const addTask = e => {
+		if (e.keyCode === 13 && inputValue !== "") {
+			const newTask = [tasks.length + 1, inputValue];
+			setTasks([...tasks, newTask]);
+			setInputValue("");
+		}
+	};
+
+	const deleteTask = k => {
+		const newTasks = tasks.filter(task => task[0] !== k);
+		setTasks(newTasks);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<input
+				className="my-3"
+				type="text"
+				onChange={e => setInputValue(e.target.value)}
+				value={inputValue}
+				onKeyDown={addTask}
+				placeholder={inputValue === "" ? "Add a new task..." : ""}
+			/>
+
+			<ul className="p-0">
+				{tasks.map(task => (
+					<>
+						<li
+							className="card bg-success text-left"
+							onClick={() => deleteTask(task[0])}
+							key={task[0]}
+							style={{ listStyleType: "none" }}>
+							{task[1]}
+						</li>
+						<hr />
+					</>
+				))}
+			</ul>
+			<hr style={{ borderTop: "3px dashed" }} />
+			<small className="mb-3">
+				{tasks.length > 0 ? (
+					<b>{tasks.length} tasks pendings!!</b>
+				) : (
+					<b>Nothing to do! Yuju!</b>
+				)}
+			</small>
+		</>
 	);
-}
+};
